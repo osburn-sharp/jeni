bd6fc8caa2dfe8e9746ccca00fe21257cd41eba3
Robert Sharp <robert@osburn-sharp.ath.cx>
2012-10-04 20:34:45 +0100

    Application forked from local jeni due to Gam-space name clash.

--- lib/jeni/version.rb
+++ lib/jeni/version.rb
@@ -1,13 +1,13 @@
 # Created by Jevoom
 #
-# 04-Oct--2012
+# 04-Oct-2012
 #   Application forked from local 'jeni' due to Gam-space name clash.
 
 module Jeni
   # version set to 0.2.0
   Version = '0.2.0'
-  # date set to 04-Oct--2012
-  Version_Date = '04-Oct--2012'
-  #ident string set to: jeni-0.2.0 04-Oct--2012
-  Ident = 'jeni-0.2.0 04-Oct--2012'
+  # date set to 04-Oct-2012
+  Version_Date = '04-Oct-2012'
+  #ident string set to: jeni-0.2.0 04-Oct-2012
+  Ident = 'jeni-0.2.0 04-Oct-2012'
 end
